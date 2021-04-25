7a10e042b62e4b650e50d5aead23a036741fa40b
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-04-25 00:06:44 -0600

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -12,8 +12,6 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://vitejs.dev/guide/api-hmr.html
 if (import.meta.hot) {
   import.meta.hot.accept();
 }
