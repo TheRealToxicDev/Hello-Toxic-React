57109a75ecddedd1aff2bf07d47f0c5bc2a74459
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-04-24 23:18:36 -0600

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -39,7 +39,7 @@ export default function Home() {
       </animated.div>
       <div className="navigation">
         <animated.div onMouseEnter={trigger}>
-          <a className="btn--click-me" onClick={handleChangeHello}>
+          <a className="btn--click-me" onClick={handleChangeHello} style={{color: '#7289DA'}}>
             Psst, click me
           </a>
         </animated.div>
